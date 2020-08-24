736b8a1a154f0f4a9cb2bbfbe09c4c54c0a944d9
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-24 11:50:36 -0400

    Linting issue

--- __tests__/ActionList/ListItem.test.tsx
+++ __tests__/ActionList/ListItem.test.tsx
@@ -1,22 +1,22 @@
-import { render, cleanup } from 'react-native-testing-library';
+import {render, cleanup} from 'react-native-testing-library';
 import React from 'react';
 
 import ListItem from '../../src/ActionList/ListItem';
-import { ActionListItemProps } from '../../src/ActionList/ListTypes';
+import {ActionListItemProps} from '../../src/ActionList/ListTypes';
 
 afterEach(cleanup);
 
 describe('<ListItem />', () => {
-    const actionItem: ActionListItemProps = {
-        title: 'Edit Account',
-        route: 'EditAccount',
-    };
+  const actionItem: ActionListItemProps = {
+    title: 'Edit Account',
+    route: 'EditAccount',
+  };
 
-    it('should match snapshot', () => {
-        const rendered = render(
-            <ListItem title={actionItem.title} route={actionItem.route} />,
-        ).toJSON();
+  it('should match snapshot', () => {
+    const rendered = render(
+      <ListItem title={actionItem.title} route={actionItem.route} />
+    ).toJSON();
 
-        expect(rendered).toMatchSnapshot();
-    });
+    expect(rendered).toMatchSnapshot();
+  });
 });
